71ea4b24678fda61b0cc2786fa92aab9989363f2
marcobianco20 <marco.bianco.0120@gmail.com>
2026-03-17 14:49:47 +0100

    Commit my Python code

--- main.py
+++ main.py
@@ -6,33 +6,37 @@
 # See the solution video in the 100 Days of Python Course for explainations.
 
 
-from datetime import datetime
-import pandas
-import random
+import datetime as dt
 import smtplib
+import pandas as pd
+import random
 import os
 
-# import os and use it to get the Github repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
+today = dt.datetime.now()
 today_tuple = (today.month, today.day)
+# today_month = today.month
+# today_day = today.day
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
+data = pd.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
 if today_tuple in birthdays_dict:
     birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+    #Read letter content
+    with open(file_path) as file:
+        content = file.read()
+        content = content.replace("[NAME]",birthday_person["name"])
+
+    #Send email
+    with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
         connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+             from_addr=MY_EMAIL,
+             to_addrs=birthday_person["email"],
+             msg=f"Subject:Happy Birthday!\n\n{content}")
